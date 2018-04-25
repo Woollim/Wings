059ef23a984e4ad270a97efe2edcdf107c659959
W-Vertex <qudcks9191@naver.com>
2018-04-25 23:56:57 +0900

    modify connection logic

--- app/src/main/java/w_vertex/wings/NetworkManager.kt
+++ app/src/main/java/w_vertex/wings/NetworkManager.kt
@@ -1,9 +1,12 @@
 package w_vertex.wings
 
+import io.reactivex.*
+import io.reactivex.disposables.*
+import io.reactivex.schedulers.*
 import io.reactivex.subjects.*
 import java.io.*
 import java.net.*
-
+import java.util.concurrent.*
 
 
 /**
@@ -12,49 +15,58 @@ import java.net.*
 class NetworkManager(val ip: String, val port: Int = 3000) {
 
     var socket: Socket? = null
-    private var thread: Thread? = null
 
     val dataSubject: PublishSubject<String>
     val stateSubject: BehaviorSubject<Boolean>
 
+    private val disposables by lazy { CompositeDisposable() }
+
     init {
-        createSocket()
         dataSubject = PublishSubject.create()
         stateSubject = BehaviorSubject.createDefault(false)
+        createSocket()
     }
 
-    fun createSocket(){
-        Thread{
-            try { socket = Socket(ip, port); readData(); true }
-            catch (e: IOException){ false }
-                    .let { stateSubject.onNext(it) }
+    fun createSocket() {
+        Thread {
+            try {
+                socket = Socket(ip, port)
+                readData(); true
+            } catch (e: IOException) {
+                false
+            }.let { stateSubject.onNext(it) }
         }.start()
     }
 
-    private fun readData(){
-        socket?.apply {
-            thread = Thread{
-                while (true){
-                    val data = ByteArray(1024)
-                    try { this.getInputStream().read(data) }
-                    catch (e: IOException){ break }
-                    if (data.size == 1024){ break }
-                    else{ dataSubject.onNext(String(data)) }
-                }
-                stateSubject.onNext(false)
-            }.apply { start() }
-        }
+    private fun readData() {
+        Observable.interval(100, TimeUnit.MILLISECONDS)
+                .repeat()
+                .subscribeOn(Schedulers.io())
+                .map { socket!! }
+                .filter { it.isConnected }
+                .map { it.getInputStream() }
+                .filter { it.available() > 0 }
+                .map {
+                    val byteArray = ByteArray(it.available())
+                    it.read(byteArray)
+                    String(byteArray)
+                }.subscribe{ dataSubject.onNext(it) }
+                .let { disposables.add(it) }
     }
 
-    fun sendData(msg: String){
+    fun sendData(msg: String) {
+        Observable.just(msg)
+                .subscribeOn(Schedulers.io())
+                .filter { socket != null }
+                .subscribe { socket!!.getOutputStream().write(msg.toByteArray()) }
+    }
+
+    fun close() {
         socket?.let {
-            Thread{ it.getOutputStream().write(msg.toByteArray()) }.start()
+            disposables.clear()
+            stateSubject.onNext(false)
+            socket = null
         }
-    }
-
-    fun close(){
-        socket?.close()
-        socket = null
     }
 
 }
